0812ea83b8eabb7ffd34d2160f79b8939861d449
Dendi Maniac <daq30899@gmail.com>
2020-11-01 01:09:54 +0200

    Clean up react app logic + no longer use function authentication

--- src/Chat/index.js
+++ src/Chat/index.js
@@ -2,38 +2,43 @@ import React, { useState, useEffect, useRef } from "react";
 import { HubConnectionBuilder } from "@microsoft/signalr";
 import axios from "axios";
 
-import ChatWindow from "./ChatWindow/ChatWindow";
-import ChatInput from "./ChatInput/ChatInput";
+import ChatWindow from "./ChatWindow";
+import ChatInput from "./ChatInput";
 
+const FunctionURL = "https://privatekeyboard.azurewebsites.net/api";
 const Chat = () => {
   const [chat, setChat] = useState([]);
   const latestChat = useRef(null);
+  
   latestChat.current = chat;
   useEffect(() => {
     const connection = new HubConnectionBuilder()
-      .withUrl("http://localhost:7071/api")
+      .withUrl(`${FunctionURL}`)
       .withAutomaticReconnect()
       .build();
+
+    connection.on("newMessage", (message) => {
+      console.log("ReceiveMessage", message);
+      const updatedChat = [...latestChat.current];
+      updatedChat.push(message);
+      setChat(updatedChat);
+    });
+
     connection
       .start()
-      .then((result) => {
+      .then(() => {
         console.log("Connected!");
-        connection.on("newMessage", (message) => {
-          console.log("ReceiveMessage", message);
-          const updatedChat = [...latestChat.current];
-          updatedChat.push(message);
-          setChat(updatedChat);
-        });
       })
       .catch((e) => console.log("Connection failed: ", e));
   }, []);
+
   const sendMessage = async (user, message) => {
     const chatMessage = {
       user: user,
       message: message,
     };
     try {
-      return await axios.post("http://localhost:7071/api/messages", {
+      return await axios.post(`${FunctionURL}/messages`, {
         sender: chatMessage.user,
         text: chatMessage.message,
       });
