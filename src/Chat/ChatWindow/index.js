9f0fb4f15f767243acd0bc0867f1410f44a6f9ab
Dendi Maniac <daq30899@gmail.com>
2020-11-01 02:37:45 +0200

    Fixed message display issue

--- src/Chat/ChatWindow/index.js
+++ src/Chat/ChatWindow/index.js
@@ -7,8 +7,8 @@ const ChatWindow = (props) => {
   const chat = props.chat.map((m) => (
     <Message
       key={Date.now() * Math.random()}
-      user={m.user}
-      message={m.message}
+      sender={m.sender}
+      text={m.text}
     />
   ));
   return <div>{chat}</div>;
