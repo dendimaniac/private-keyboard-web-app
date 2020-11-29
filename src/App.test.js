c3bf7c792de61b7841b8344591c6b23798e72d3a
Vy Nguyen <vynguyen9699@gmail.com>
2020-11-29 18:05:36 +0200

    add more test cases for chat and chat input

--- src/App.test.js
+++ src/App.test.js
@@ -1,7 +1,15 @@
-import React from 'react';
-import App from './App';
+import React from "react";
 import { shallow } from "enzyme";
 
-it("renders without crashing", () => {
-  shallow(<App />);
+import App from "./App";
+import Chat from "./Chat";
+
+describe("<App />", () => {
+  it("renders without crashing", () => {
+    shallow(<App />);
+  });
+  it("renders one <Chat/> components", () => {
+    const app = shallow(<App />);
+    expect(app.find(Chat)).toHaveLength(1);
+  });
 });
